63a518cb7a2d29accbaa98956351d12b29109533
ghalamdoost.shahab <ghalamdoost.shahab@live.com>
2021-03-06 14:48:17 -0500

    Task7 after merge

--- app_server/models/food.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const foodSchema = new mongoose.Schema({
-    name: { type: String, required: true, minLength: 3 },
-    type:{type:String, required:true}
-});
