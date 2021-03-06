ffb066b825553915a0396ff635730cc2e91bcf73
ghalamdoost.shahab <ghalamdoost.shahab@live.com>
2021-03-06 15:49:15 -0500

    Task8

--- app_server/controllers/food.js
+++ app_server/controllers/food.js
@@ -1,24 +1,47 @@
-const foodArray=[
-    {name:"Oatmeal", type:"Breakfast"},
-    {name:"Steak", type:"Lunch"},
-    {name:"Salad", type:"Dinner"}
-]
 
-const myFavFood="Steak";
-
-
-/* Get home page */
-const foodList = function(req, res){
-    res.render('foodlist', {title: 'Food List', foods:foodArray});
+// const { request } = require("express");
+const request = require('request');
+const apiOptions = {
+  server: 'http://localhost:3000'
 };
 
-const myFavourteFood = function(req, res){
-    res.render('myfavourite-food', {title: 'My Favourte Food', food:myFavFood});
+const _renderHomepage=(req,res,responseBody)=>{
+    res.render('foodlist',{
+        foods:responseBody
+    });
 };
 
+const homelist=(req,res)=>{
+    const path='/api/foods';
+    const requestOptions={
+        url:apiOptions.server+path,
+        method:'GET',
+        json:{}
+    };
+    request(requestOptions, (err, response, body)=>{
+        _renderHomepage(req,res,body);
+    });
+};
 
+const _renderDetailPage=(req,res,responseBody)=>{
+    res.render('food-info',{
+        currentFood: responseBody
+    });
+};
+
+const foodInfo=(req,res)=>{
+    const path=`/api/foods/${req.params.foodid}`;
+    const requestOptions={
+        url:apiOptions.server+path,
+        method:"GET",
+        json:{}
+    };
+    request(requestOptions,(err,response,body)=>{
+        _renderDetailPage(req,res,body);
+    });
+};
 
 module.exports = {
-    foodList,
-    myFavourteFood
+    homelist,
+    foodInfo
 };  
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const ctrMain = require('../controllers/main');
+// const ctrMain = require('../controllers/main');
 const ctrlFood = require('../controllers/food');
 
 
 /* GET home page. */
-router.get('/', ctrMain.index);
+router.get('/', ctrlFood.homelist);
 
 /* GET foodList page. */
-router.get('/list', ctrlFood.foodList);
+router.get('/foods/:foodid', ctrlFood.foodInfo);
 
-/* GET myFavouriteFood page. */
-router.get('/favourite', ctrlFood.myFavourteFood);
 
 module.exports = router;
